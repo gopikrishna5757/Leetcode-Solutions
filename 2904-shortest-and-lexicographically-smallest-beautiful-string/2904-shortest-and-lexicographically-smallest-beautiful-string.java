8efd2622bd95bcfe6b2081d3e1010026febddf4c
CHALAMALA GOPI KRISHNA <gopikrishnachalamala87@gmail.com>
2026-08-26 20:01:56 +0530

    Time: 2 ms (40.94%), Space: 44.2 MB (28.86%) - LeetHub

--- 2904-shortest-and-lexicographically-smallest-beautiful-string/2904-shortest-and-lexicographically-smallest-beautiful-string.java
+++ 2904-shortest-and-lexicographically-smallest-beautiful-string/2904-shortest-and-lexicographically-smallest-beautiful-string.java
@@ -1,53 +1,41 @@
 class Solution {
     public String shortestBeautifulSubstring(String s, int k) {
         int n = s.length();
-        int i=0,j=0;
-        int c=0;
-        StringBuilder sb= new StringBuilder();
-        while(j<n){
-            System.out.println(s.substring(i,j+1));
-            if(s.charAt(j)=='1'){
-                c++;
+        int i = 0, ones = 0;
+        String ans = "";
 
+        for (int j = 0; j < n; j++) {
+
+            if (s.charAt(j) == '1') {
+                ones++;
             }
-            while(c>k){
-               
-                if(s.charAt(i)=='1'){
-                    c--;
-                    
+
+            // More than k ones
+            while (ones > k) {
+                if (s.charAt(i) == '1') {
+                    ones--;
                 }
                 i++;
             }
-            while(c==k&&s.charAt(i)=='0'){
+
+            // Remove leading zeros
+            while (ones == k && s.charAt(i) == '0') {
                 i++;
             }
-            if(c==k){
-                String cur=s.substring(i,j+1);
-                
-                if(sb.length()==0){
-                    sb.append(s.substring(i,j+1));
-                }
-              
-                else if(sb.length()>cur.length()){
-                    sb.setLength(0);
-                    sb.append(cur);
-                 
-                    
 
-                }
-                else if(cur.length()==sb.length()){
-                       if(cur.compareTo(sb.toString())<0){
-                        sb.setLength(0);
-                        sb.append(cur);
-                    }
-                 
+            if (ones == k) {
+                String current = s.substring(i, j + 1);
+
+                if (ans.isEmpty()
+                        || current.length() < ans.length()
+                        || (current.length() == ans.length()
+                            && current.compareTo(ans) < 0)) {
+
+                    ans = current;
                 }
             }
-            j++;
-            
         }
-       
-        
-        
-   return sb.toString(); }
+
+        return ans;
+    }
 }

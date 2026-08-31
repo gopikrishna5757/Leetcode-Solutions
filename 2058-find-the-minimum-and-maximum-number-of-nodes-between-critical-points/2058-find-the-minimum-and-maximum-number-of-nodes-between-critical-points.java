11747424c3aa27050c8e8086db30fdc62c0d5615
CHALAMALA GOPI KRISHNA <gopikrishnachalamala87@gmail.com>
2026-08-31 15:01:39 +0530

    Time: 8 ms (23.96%), Space: 105.6 MB (97.58%) - LeetHub

---
+++ 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.java
@@ -0,0 +1,51 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public int[] nodesBetweenCriticalPoints(ListNode head) {
+        if (head == null || head.next == null || head.next.next == null)
+            return new int[] { -1, -1 };
+        ListNode temp = head;
+        temp = temp.next;
+        int min = Integer.MAX_VALUE;
+        int count1 = -1, count2 = -1, count3 = -1, k = 2;
+
+        while (temp.next != null) {
+            if ((temp.val > head.val && temp.val > temp.next.val)
+                    || (temp.val < head.val && temp.val < temp.next.val)) {
+                if (count1 == -1) {
+                    count1 = k;
+                } else if (count2 == -1) {
+                    count2 = k;
+                    count3 = k;
+
+                } else {
+                    min = Math.min(min, k - count3);
+                    count3 = k;
+                }
+            }
+
+            k++;
+            temp = temp.next;
+            head = head.next;
+        }
+        System.out.println(count1);
+        System.out.println(count2);
+        System.out.println(count3);
+
+        if (count2 == -1 || count1 == -1)
+            return new int[] { -1, -1 };
+        if (count3 == -1) {
+            return new int[] { count2 - count1, count2 - count1 };
+        }
+
+        return new int[] { Math.min(count2 - count1, min), count3 - count1 };
+    }
+}

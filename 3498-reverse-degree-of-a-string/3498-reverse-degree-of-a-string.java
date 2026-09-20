defdb724894515eba4a75321ba34ce9fd7631273
CHALAMALA GOPI KRISHNA <gopikrishnachalamala87@gmail.com>
2026-09-20 12:39:30 +0530

    Time: 1 ms (100%), Space: 44.3 MB (10.65%) - LeetHub

---
+++ 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int reverseDegree(String s) {
+        int n = s.length();
+        int sum=0;
+        for(int i=0;i<n;i++){
+            sum+=(i+1)*(26-(s.charAt(i)-'a'));
+        }
+        
+    return sum;}
+}
